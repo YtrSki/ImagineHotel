189320e52c7daf3dc84746d63e5274d284f4661d
yut0124 <45488423+yut0124@users.noreply.github.com>
2021-01-22 11:25:19 +0900

    Initial commit

---
+++ UCD/TopScreen/CheckInViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CheckInViewController.swift
+//  UCD
+//
+//  Created by YutaroSakai on 2021/01/22.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ UCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UCD
+//
+//  Created by YutaroSakai on 2021/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
